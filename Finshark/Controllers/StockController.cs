551b255f9c784ab9fa31b415dca6fd511388e1e8
Milos Ursulovic <milosursulovic2696@gmail.com>
2025-01-07 19:39:56 +0100

    Create update method for stocks

--- Finshark/Controllers/StockController.cs
+++ Finshark/Controllers/StockController.cs
@@ -44,12 +44,41 @@ namespace Finshark.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create([FromBody] CreateStockRequest stockDto)
+        public IActionResult Create([FromBody] CreateStockRequest createDto)
         {
-            var stockModel = stockDto.ToStockFromCreateDto();
-            _context.Stocks.Add(stockModel);
+            var stockModel = createDto.ToStockFromCreateDto();
+            
+            _context
+                .Stocks
+                .Add(stockModel);
+
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequest updateDto)
+        {
+            var stockModel = _context
+                .Stocks
+                .FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+
+            _context.SaveChanges();
+
+            return Ok(stockModel.ToStockDto());
         }
     }
 }
