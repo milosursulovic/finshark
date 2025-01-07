ddca8be954ecc99597d702d06ee18e1d54d3d48e
Milos Ursulovic <milosursulovic2696@gmail.com>
2025-01-07 13:02:59 +0100

    Add get routes for stocks

---
+++ Finshark/Controllers/StockController.cs
@@ -0,0 +1,37 @@
+﻿using Finshark.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finshark.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public StockController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _applicationDbContext.Stocks.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _applicationDbContext.Stocks.Find(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);
+        }
+    }
+}
