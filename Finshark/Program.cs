f8c655a670a4a0f582c1804f34432f9333880076
Milos Ursulovic <milosursulovic2696@gmail.com>
2025-01-12 19:17:54 +0100

    Refactor

--- Finshark/Program.cs
+++ Finshark/Program.cs
@@ -5,10 +5,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
