5f723cd78f3c162f9835d9a20e2a7e6e55c1282d
Milos Ursulovic <milosursulovic2696@gmail.com>
2025-01-18 12:18:31 +0100

    Add sorting

--- Finshark/Repository/StockRepository.cs
+++ Finshark/Repository/StockRepository.cs
@@ -62,6 +62,16 @@ namespace Finshark.Repository
                 stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
             }
 
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = query.IsDescending ?
+                        stocks.OrderByDescending(s => s.Symbol) :
+                        stocks.OrderBy(s => s.Symbol);
+                }
+            }
+
             return await stocks.ToListAsync();
         }
 
