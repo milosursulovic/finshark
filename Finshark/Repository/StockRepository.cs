8a22fce3b82870e0ac81440bcea84352a21b035b
Milos Ursulovic <milosursulovic2696@gmail.com>
2025-01-18 12:30:37 +0100

    Add pagination

--- Finshark/Repository/StockRepository.cs
+++ Finshark/Repository/StockRepository.cs
@@ -72,7 +72,12 @@ namespace Finshark.Repository
                 }
             }
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await stocks
+                .Skip(skipNumber)
+                .Take(query.PageSize)
+                .ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
