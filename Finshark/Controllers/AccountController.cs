32422c2a02fd3a827f1c68efe751563123fa5f1d
Milos Ursulovic <milosursulovic2696@gmail.com>
2025-01-19 13:30:50 +0100

    Create login route for user

--- Finshark/Controllers/AccountController.cs
+++ Finshark/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Finshark.Interfaces;
 using Finshark.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Finshark.Controllers
 {
@@ -12,15 +13,56 @@ namespace Finshark.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        public AccountController(
+            UserManager<AppUser> userManager,
+            ITokenService tokenService,
+            SignInManager<AppUser> signInManager
+        )
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
         }
 
         // Test user email:     investor444@gmail.com
         // Test user password:  4j9eZyk(&RiJJaU
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager
+                .Users
+                .FirstOrDefaultAsync(u => u.UserName == loginDto.Username.ToLower());
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid username!");
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded)
+            {
+                return Unauthorized("Username not found and/or password incorrect!");
+            }
+
+            return Ok
+                (
+                    new NewUserDto
+                    {
+                        Username = user.UserName,
+                        Email = user.Email,
+                        Token = _tokenService.CreateToken(user)
+                    }
+                );
+        }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
