451ff7b6cbdd124358dd19b8fb0058a5a7350a00
Milos Ursulovic <milosursulovic2696@gmail.com>
2025-01-21 19:12:24 +0100

    Bound app user to comment

--- Finshark/Models/Comment.cs
+++ Finshark/Models/Comment.cs
@@ -12,5 +12,7 @@ namespace Finshark.Models
         public int? StockId { get; set; }
         // Navigation property
         public Stock? Stock { get; set; }
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
